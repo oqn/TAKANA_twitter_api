1ca3a3e02e4dbc21b2d876f4a9b72a3a500a3ca6
YOMOGItaro <yomogitaro@gmail.com>
2010-05-18 08:51:48 +0900

    mousugu tlX

--- spctwitter.rb
+++ spctwitter.rb
@@ -1,29 +1,27 @@
-# -*- coding: utf-8 -*-
-
 #!/usr/bin/ruby
 # -*- coding: utf-8 -*-
 require 'rubygems'
 require 'twitter'
 require 'MeCab'
 
-def tweet(text)
+class TAKAKI
+  def initialize
+    consumer_token_key="FtsHUY8yaSiMJpWOfTzuUw"
+    consumer_secret="En0JVvDNx0WI1C2zp56gGH0ZcuNNOLUxRmLGg1pC4"
+    
+    access_token_key="42127145-DBLQ0FYJB40JMajXDAMVacUy7PNmCloeZTZqG472M"
+    access_secret="msOgzeK2R3E2ClZ3DZIPrFQkzIjmg3V0ycHtN7Jzhg"
+    
+    oauth = Twitter::OAuth.new(consumer_token_key, consumer_secret)
+    oauth.authorize_from_access(access_token_key, access_secret)
+    @client = Twitter::Base.new(oauth)
+  end
 
-  consumer_token_key="yvGpZMJqyYiypgjAFCN4w"
-  consumer_secret="WwfbiQzD5VZxqTO51Nq5UFA5JjcFPQzlvrq2Lrrs"
-  
-  access_token_key="42127145-DBLQ0FYJB40JMajXDAMVacUy7PNmCloeZTZqG472M"
-  access_secret="msOgzeK2R3E2ClZ3DZIPrFQkzIjmg3V0ycHtN7Jzhg"
-
-  oauth = Twitter::OAuth.new(consumer_token_key, consumer_secret)
-  oauth.authorize_from_access(access_token_key, access_secret)
-
-  client = Twitter::Base.new(oauth)
-  client.update(text)
-#  puts ("**recent tweets**")
-#  sentence = client.friends_timeline[0].text
-#  c = MeCab::Tagger.new(ARGV.join(" ")) 
-#  puts c.parse(sentence)
-
-#  puts client.friends_timeline[0].user.name
-
+  def tweet(text)
+    @client.update(text)
+  end
+ 
+  def timeline
+    @client.friends_timeline
+  end
 end
--- twiform.rb
+++ twiform.rb
@@ -3,11 +3,12 @@
 #
 # This file is gererated by ruby-glade-create-template 1.1.4.
 #
+
 require 'libglade2'
 require 'spctwitter.rb'
 require 'MeCab'
 
-class TwiformGlade
+class TAKANA_mainform
   include GetText
 
   attr :glade
@@ -15,27 +16,47 @@ class TwiformGlade
   def initialize(path_or_data, root = nil, domain = nil, localedir = nil, flag = GladeXML::FILE)
     bindtextdomain(domain, localedir, nil, "UTF-8")
     @glade = GladeXML.new(path_or_data, root, domain, localedir, flag) {|handler| method(handler)}
-  end
+  end  
   
   def on_wMain_destroy(widget)
-    gtk_main_quit()
+    p 'bye'
+    Gtk.main_quit
   end
-  
 
+  def on_bTweets_clicked(widget)
+    begin
+      @t.tweet(@glade['tvTweet'].buffer.text)
+      @glade['lEtc'].text = "(tweet successful)"+@glade['tvTweet'].buffer.text
+      @glade['tvTweet'].buffer.text = ""
+    rescue
+      @glade['lEtc'].text = "(tweet unsuccessful)"
+    end
+  end
+
+  def on_bMyTweets_clicked(widget)
+    tltxt = @t.timeline
+    #p tltxt
+    @glade['tvResponce'].buffer.text = ''
+    tltxt.each{|tlt|
+      @glade['tvResponce'].buffer.text += tlt.text+"\n"
+    }
+    @glade['lEtc'].text = "(showing timeline)"
+    
+    #pp @t.user_timeline
+    #puts '*'*50
+    
+    #pp @t.replies
+    #puts '*'*50
+    puts "on_bMyTweets_clicked() is not implemented yet."
+  end
 
   def on_bLogin_clicked(widget)
-    tweet(@glade['tvConsole'].buffer.text)
-    p @glade
-    view = @glade['tvConsole'].buffer.text+="test\n"
+    @t = TAKAKI.new
+    @glade['lEtc'].text = "(login successful)"
   end
-end
 
-# Main program
-if __FILE__ == $0
-  # Set values as your own application. 
-  PROG_PATH = "twiform.glade"
-  PROG_NAME = "YOUR_APPLICATION_NAME"
-  o = TwiformGlade.new(PROG_PATH, nil, PROG_NAME)
-  o.glade["wMain"].show_all
-  Gtk.main
+  def on_bReply_clicked(widget)
+    puts "on_bReply_clicked() is not implemented yet."
+  end
+  
 end
